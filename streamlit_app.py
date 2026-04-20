33843734c10381967dc0b62b51c0e4c583097fad
Christopher Argenis Preciado Silva <christopher.preciado.silva@uabc.edu.mx>
2026-04-20 01:33:35 -0700

    Fix syntax error in RTC configuration

--- streamlit_app.py
+++ streamlit_app.py
@@ -235,7 +235,9 @@ if mode == "Live Demo Webcam":
     },
     rtc_configuration={
         "iceServers": [
-            {"urls": ["stun:stun.l.google.com:19302"]}
+            {"urls": ["stun:stun.l.google.com:19302"]},
+            {"urls": ["stun:stun1.l.google.com:19302"]},
+            {"urls": ["stun:stun2.l.google.com:19302"]},
         ]
     },
     async_processing=True,
